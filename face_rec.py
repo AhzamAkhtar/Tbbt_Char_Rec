b8643ff5078401b6e3780123438c2f3dbb08f6a2
Ahzam Akhtar <ahzam8186@gmail.com>
2021-12-18 01:28:36 +0530

    more pictures added

--- face_rec.py
+++ face_rec.py
@@ -3,7 +3,7 @@ haar_cascade=cv.CascadeClassifier("haar_face.xml")
 people=["lenord","sheldon"]
 face_recognizer=cv.face.LBPHFaceRecognizer_create()
 face_recognizer.read("face_tbbt.yml")
-img=cv.imread(r"C:\Users\ahzam\PycharmProjects\tbbt\val\sheldon\the-big-bang-theory-final-sheldon-cooper-jim-parsons-1561048451.jpg")
+img=cv.imread(r"C:\Users\ahzam\PycharmProjects\tbbt\val\sheldon\69.jpg")
 gray=cv.cvtColor(img,cv.COLOR_BGR2GRAY)
 cv.imshow("person",gray)
 face_rect=haar_cascade.detectMultiScale(gray,scaleFactor=1.1,minNeighbors=4)
@@ -12,9 +12,10 @@ for (x,y,w,h) in face_rect:
     label,confidence=face_recognizer.predict(faces_roi)
     print(f"label={people[label]} with a confidence of {confidence}")
 
-    cv.putText(img,str(people[label]),(20,20),cv.FONT_HERSHEY_COMPLEX,1.0,(0,255,0),thickness=2)
+    cv.putText(img,str(people[label]),(100,100),cv.FONT_HERSHEY_COMPLEX,1.0,(0,255,0),thickness=2)
     cv.rectangle(img,(x,y),(x+w,y+h),(0,255,0),thickness=2)
 cv.imshow("Dect",img)
 cv.waitKey(0)
+
 
 
